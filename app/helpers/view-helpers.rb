85027689f0b5445b0de8e0d6a93282195ef2e6b9
Alex Kukushkin <alex@kukushk.in>
2014-02-25 12:44:54 +0200

    Redesigned user login form.

--- app/helpers/view-helpers.rb
+++ app/helpers/view-helpers.rb
@@ -1,8 +1,18 @@
 
+# Returns A HREF link to a login using given provider.
+#
 def link_to_social_login( provider_key, text = nil, on_success = nil )
   provider = Aerogel::Auth.providers[provider_key] || {}
   text ||= provider[:name]
+  link_to url_to_social_login( provider_key, on_success ), text.to_s, title: "log in using #{provider[:name]}"
+end
+
+
+# Returns URL to a login using given provider.
+#
+def url_to_social_login( provider_key, on_success = nil )
+  provider = Aerogel::Auth.providers[provider_key] || {}
   origin = on_success || params['on_success']
   query_string = origin ? "?origin=#{origin}" : ''
-  link_to "/auth/#{provider_key}#{query_string}", text.to_s, title: "log in using #{provider[:name]}"
+  "/auth/#{provider_key}#{query_string}"
 end
